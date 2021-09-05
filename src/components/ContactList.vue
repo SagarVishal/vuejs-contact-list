569993e2d1fc965c050158b421c15bc9dda9ba0a
Vishal Sagar <vishalsagar.v7@gmail.com>
2021-09-05 12:54:51 +0530

    Setup store and initial ui structure

---
+++ src/components/ContactList.vue
@@ -0,0 +1,69 @@
+<template>
+  <b-container>
+    <b-row align-h="center">
+      <b-col col lg="8">
+        <b-card
+          class="custom-card w-100 mt-5"
+          header-tag="header"
+          header-class="custom-header dfaisb bb-1"
+          body-class="custom-body p-0"
+        >
+          <template #header>
+            <h5>Contact List</h5>
+          </template>
+
+          <div>{{ contactList }}here</div>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "ContactList",
+  beforeMount() {
+    this.$store.dispatch("getContactList");
+  },
+  computed:{
+    contactList: function() {
+      return this.$store.getters.contactList
+    }
+  }
+};
+</script>
+
+<style>
+.custom-card {
+  border: none;
+  background-color: #fff;
+  border-radius: 0.625rem;
+  box-shadow: 0 2px 0 rgba(90, 97, 105, 0.11), 0 4px 8px rgba(90, 97, 105, 0.12),
+    0 10px 10px rgba(90, 97, 105, 0.06), 0 7px 70px rgba(90, 97, 105, 0.1);
+}
+
+.custom-card .custom-header {
+  box-shadow: none;
+  padding: 1rem 1rem;
+  border-radius: 0.625rem 0.625rem 0 0;
+  background-color: white;
+}
+
+.dfaisb {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.bb-1 {
+  border-bottom: 1px solid #e1e5eb;
+}
+
+.custom-card > .custom-body {
+  border-radius: 0.625rem 0.625rem 0 0;
+}
+
+.custom-card .custom-header + .custom-body {
+  border-radius: 0 0;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios'
 
 Vue.use(Vuex);
+Vue.use(axios)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    contactList: {},
+  },
+  mutations: {
+    contactList(state, data) {
+      state.contactList = data;
+    },
+  },
+  actions: {
+    getContactList() {
+      return axios.get("https://reqres.in/api/users").then((response) => {
+        this.commit("contactList", response.data);
+      });
+    },
+  },
+  getters: {
+    contactList(state) {
+      return state.contactList
+    }
+  },
 });
